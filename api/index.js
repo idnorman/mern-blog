14f2a8fad28b0dd848180392b857a18695fa843d
Norman <norman.primary@gmail.com>
2024-01-22 07:20:20 +0700

    add mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+import { error } from "console";
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((error) => {
+    console.log("MongoDB failed to connect: " + error);
+  });
 
 const app = express();
 
