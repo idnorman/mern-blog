fc2d8258241d29580f925a0ceeb3fdeee309b640
Norman <norman.primary@gmail.com>
2024-01-22 17:48:53 +0700

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+    email,
+  });
+
+  try {
+    await newUser.save();
+    res.json("Signup Successfull");
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
