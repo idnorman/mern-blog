5bed5c1e622537500e1e5314b8e27dd1e93e55c3
Norman <norman.primary@gmail.com>
2024-02-11 07:28:48 +0700

    add update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser'
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
